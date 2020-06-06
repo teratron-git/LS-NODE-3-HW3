a149506c3389bdb8ea9bffc1bb839d7ee71cf156
Tereshkov Sergei[D] <rat87@mail.ru>
2020-06-06 21:42:00 +0300

    fix: errorHandler error

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   err.status = 404;
   next(err);
 });
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', { message: err.message, error: err });
 });
